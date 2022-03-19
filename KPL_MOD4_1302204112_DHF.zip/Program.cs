d00bd1062c0691c93206a6359c437975e32ed438
rifkyfirdaus28 <100988194+rifkyfirdaus28@users.noreply.github.com>
2022-03-19 15:13:02 +0700

    Add files via upload

---
+++ KPL_MOD4_1302204112_DHF.zip/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JurnalMod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // memanggil generic method
+
+            // memvalidasi generic class
+  
+        }
+    }
+}
